5a21a99c4b4de410160a3e60f5d54f3b3006edce
gomarcd <git@gomarcd.dev>
2023-04-02 16:36:18 -0600

    add basic example of taking user input and returning response from gpt

--- app/app.py
+++ app/app.py
@@ -1,13 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 import os
 import openai
 import json
+import time
 
 app = Flask(__name__)
 
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
 @app.route('/', methods=['GET', 'POST'])
-def welcome():
-    return 'hello'
+def chat():
+    start_time = time.time()
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": request.json["input_text"]},
+        ],
+        temperature=0,
+    )
+    end_time = time.time()
+    duration = end_time - start_time
+    duration = round(duration, 2)
+    response = "\n" +response["choices"][0]["message"]["content"] + "\n" + "Load:" + str(duration) + "sec" + "\n" + "\n"
+    return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
